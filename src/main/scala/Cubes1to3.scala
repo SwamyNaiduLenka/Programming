646321fe35fefea30cef5402e9808d036fb01aaf
Swamy Naidu <swamynaidu45@gmail.com>
2024-04-07 19:57:51 +0530

    Cubes1to3 program  to Scala Programming Practice

---
+++ src/main/scala/Cubes1to3.scala
@@ -0,0 +1,10 @@
+object Cubes1to3 extends App {
+//Print the cube of numbers from 1 to 3 using a "while" loop.
+
+  var l=3
+  var i=1
+  while(i<=3) {
+  println(s"Cube of $i is",i*i*i)
+    i=i+1
+    }
+}
